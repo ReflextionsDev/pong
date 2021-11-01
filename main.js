61518ecf8c0c1c228b724211efdc3e91e26c3f1f
ReflextionsDev <ReflextionsDev@gmail.com>
2021-11-01 16:00:31 -0400

    Add player paddle movement

--- main.js
+++ main.js
@@ -13,49 +13,117 @@ const BALL_SIZE = 20;
 const computerPaddle = document.querySelector('.computer-paddle');
 
 // The y-velocity of the computer paddle
-let computerPaddleYPosition = 0;
-let computerPaddleYVelocity = 1;
+let computerPaddleYPosition = 0
+let computerPaddleYVelocity = 1
+
+// Get the player paddle element
+const playerPaddle = document.querySelector('.player-paddle');
+
+// The y-velocity of the player paddle
+let playerPaddleYPosition = 0
+let playerPaddleYVelocity = 0
 
 // Update the pong world
 const ball = document.querySelector('.ball');
 
-let ballPositionX = 100;
-let ballPositionY = 170;
+const BALL_START_X = 100;
+const BALL_START_Y = 100
 
+let ballPositionX = BALL_START_X
+let ballPositionY = BALL_START_Y
 
 let ballVelocityX = 2;
 let ballVelocityY = 1;
 
-
-
 function update() {
 
+    // Test the CPU Paddle
+    if ((ballPositionX + BALL_SIZE + PADDLE_WIDTH) > GAME_AREA_WIDTH) {
 
-    // Update the ball position
-    if ((ballPositionX + BALL_SIZE) > GAME_AREA_WIDTH) {
-        ballVelocityX = -ballVelocityX
-    } else if (ballPositionX < 0) {
+        if (ballPositionY > computerPaddleYPosition && ballPositionY < (computerPaddleYPosition + PADDLE_HEIGHT)) {
+            console.log("CPU Bounce")
+            console.log(computerPaddleYPosition, ballPositionY)
+            ballVelocityX = -ballVelocityX
+        } else {
+            console.log("CPU L")
+            reset()
+        }
+
+        // Test the Player Paddle
+    } else if ((ballPositionX - PADDLE_WIDTH) < 0) {
         ballVelocityX = -ballVelocityX
     }
 
+    // Test the top and bottom boundaries
     if ((ballPositionY + BALL_SIZE) > GAME_AREA_HEIGHT) {
         ballVelocityY = -ballVelocityY
     } else if (ballPositionY < 0) {
         ballVelocityY = -ballVelocityY
     }
 
-
+    // Update the ball position
     ballPositionX += ballVelocityX
     ballPositionY += ballVelocityY
-
     ball.style.left = ballPositionX + 'px'
     ball.style.top = ballPositionY + 'px'
 
 
     // Update the computer paddle's position
-    computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity
+    if (computerPaddleYPosition + PADDLE_HEIGHT > GAME_AREA_HEIGHT) {
+        computerPaddleYVelocity = -computerPaddleYVelocity
+    } else if (computerPaddleYPosition < 0) {
+        computerPaddleYVelocity = -computerPaddleYVelocity
+    }
+
+    computerPaddleYPosition += computerPaddleYVelocity
     computerPaddle.style.top = `${computerPaddleYPosition}px`
+
+    // Update the player paddle's position
+    // Player input
+    if (input_Down) {
+        playerPaddleYVelocity = 1
+    } else if (input_Up) {
+        playerPaddleYVelocity = -1
+    } else {
+        playerPaddleYVelocity = 0
+    }
+
+    // Paddle boundaries
+    if (playerPaddleYPosition + PADDLE_HEIGHT > GAME_AREA_HEIGHT) {
+        playerPaddleYPosition = GAME_AREA_HEIGHT - PADDLE_HEIGHT
+    } else if (playerPaddleYPosition < 0) {
+        playerPaddleYPosition = 0
+    }
+
+    // Paddle movement update
+    playerPaddleYPosition += playerPaddleYVelocity
+    playerPaddle.style.top = `${playerPaddleYPosition}px`
 }
 
 // Call the update() function every 35ms
 setInterval(update, 5);
+
+// Reset
+function reset() {
+    ballPositionX = BALL_START_X
+    ballPositionY = BALL_START_Y
+}
+
+// Player controls
+let input_Down = 0, input_Up = 0
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'w') {
+        input_Up = 1
+    } else if (event.key === 's') {
+        input_Down = 1
+    }
+})
+
+document.addEventListener('keyup', (event) => {
+    if (event.key === 'w') {
+        input_Up = 0
+    } else if (event.key === 's') {
+        input_Down = 0
+    }
+})
