69eaea306eed0f33a68e8a3802142f19da6862b5
ReflextionsDev <ReflextionsDev@gmail.com>
2021-11-02 13:29:08 -0400

    Add player scoring, better CPU, organize code, better variables

--- main.js
+++ main.js
@@ -1,36 +1,59 @@
 // Size of the game area (in px)
-const GAME_AREA_WIDTH = 700;
-const GAME_AREA_HEIGHT = 500;
+const GAME_AREA_WIDTH = 700
+const GAME_AREA_HEIGHT = 500
 
 // Paddles
-const PADDLE_HEIGHT = 100;
-const PADDLE_WIDTH = 20;
+const PADDLE_HEIGHT = 100
+const PADDLE_WIDTH = 20
+const PADDLE_SPEED = 1.5
 
-const computerPaddle = document.querySelector('.computer-paddle');
-let computerPaddleYPosition = 0
-let computerPaddleYVelocity = 1
+const computerPaddle = document.querySelector('.computer-paddle')
+let computerPaddleYPosition = 250
+let computerPaddleYVelocity = 0
 
-const playerPaddle = document.querySelector('.player-paddle');
+const playerPaddle = document.querySelector('.player-paddle')
 let playerPaddleYPosition = 0
 let playerPaddleYVelocity = 0
 
 // Ball 
-const ball = document.querySelector('.ball');
-const BALL_SIZE = 20;
-const BALL_START_X = 100;
-const BALL_START_Y = 100
+const ball = document.querySelector('.ball')
+const BALL_SIZE = 20
+const BALL_START_X = 350
+const BALL_START_Y = 250
 
+const ballVelocityXGain = 0.1
+const ballVelocityYGain = 0.1
 let ballPositionX = BALL_START_X
 let ballPositionY = BALL_START_Y
-let ballVelocityX = 2;
-let ballVelocityY = 1;
+let ballVelocityX = 2
+let ballVelocityY = Math.random() < 0.5 ? -1 : 1;
 
 // Reset
 function reset() {
     ballPositionX = BALL_START_X
     ballPositionY = BALL_START_Y
-    ballVelocityX = 2;
-    ballVelocityY = 1;
+    ballVelocityX = 2
+    ballVelocityY = Math.random() < 0.5 ? -1 : 1;
+}
+
+// Scoring
+const txt_scorePlayer = document.querySelector('.score-player')
+const txt_scoreComputer = document.querySelector('.score-computer')
+
+let scorePlayer = 0
+let scoreComputer = 0
+
+function score(who) {
+    if (who === 'CPU') {
+        console.log("Player L")
+        scoreComputer++
+    } else {
+        scorePlayer++
+        console.log("CPU L")
+    }
+    txt_scorePlayer.innerHTML = "PLR: " + scorePlayer
+    txt_scoreComputer.innerHTML = "CPU: " + scoreComputer
+    reset()
 }
 
 // Player controls
@@ -57,44 +80,31 @@ setInterval(update, 5);
 
 function update() {
 
+    console.log(ballVelocityX,ballVelocityY)
+
     // #### Ball Logic ###
 
     // Test the CPU Paddle
     if ((ballPositionX + BALL_SIZE + PADDLE_WIDTH) > GAME_AREA_WIDTH) {
-
         if (ballPositionY > computerPaddleYPosition && ballPositionY < (computerPaddleYPosition + PADDLE_HEIGHT)) {
-
-            console.log("CPU Bounce")
-            console.log(computerPaddleYPosition, ballPositionY)
-
-            ballVelocityX = -ballVelocityX
+            ballVelocityX = -(Math.abs(ballVelocityX) + ballVelocityXGain)
         } else {
-            
-            console.log("CPU L")
-
-            reset()
+            score('PLR')
         }
-
     // Test the Player Paddle
     } else if ((ballPositionX - PADDLE_WIDTH) < 0) {
-
         if (ballPositionY > playerPaddleYPosition && ballPositionY < (playerPaddleYPosition + PADDLE_HEIGHT)) {
-            console.log("Player Bounce")
-            console.log(playerPaddleYPosition, ballPositionY)
-            ballVelocityX = -ballVelocityX
+            ballVelocityX = Math.abs(ballVelocityX) + ballVelocityXGain
         } else {
-            console.log("Player L")
-            reset()
+            score('CPU')
         }
-
-
     }
 
     // Test the top and bottom boundaries
     if ((ballPositionY + BALL_SIZE) > GAME_AREA_HEIGHT) {
-        ballVelocityY = -ballVelocityY
+        ballVelocityY = -(Math.abs(ballVelocityY) + ballVelocityXGain)
     } else if (ballPositionY < 0) {
-        ballVelocityY = -ballVelocityY
+        ballVelocityY = Math.abs(ballVelocityY) + ballVelocityXGain
     }
 
     // Update the ball position
@@ -107,27 +117,23 @@ function update() {
     // ### CPU Logic ###
 
     // Update the computer paddle's position
+    let computerPaddleMiddle = computerPaddleYPosition + PADDLE_HEIGHT/2
+    let computerPaddleTarget = ballPositionY + BALL_SIZE/2
 
-    // if (computerPaddleYPosition > ballPositionY) {
-    //     computerPaddleYVelocity = 0
-    //     console.log("IN RANGE", computerPaddleYVelocity)
-    // } else if (computerPaddleYPosition > ballPositionY) {
-    //     computerPaddleYVelocity = -5
-    //     console.log("BELOW BALL", computerPaddleYVelocity)
-    // } else if (computerPaddleYPosition < ballPositionY) {
-    //     computerPaddleYVelocity = 5
-    //     console.log("ABOVE BALL", computerPaddleYVelocity)
-    // } 
-    
-
+    if (computerPaddleMiddle === computerPaddleTarget) {
+        computerPaddleYVelocity = 0
+    } else if (computerPaddleMiddle < computerPaddleTarget) {
+        computerPaddleYVelocity = PADDLE_SPEED
+    } else if (computerPaddleMiddle > computerPaddleTarget) {
+        computerPaddleYVelocity = -PADDLE_SPEED
+    }
 
     // CPU boundaries
     if (computerPaddleYPosition + PADDLE_HEIGHT > GAME_AREA_HEIGHT) {
-        computerPaddleYVelocity = -computerPaddleYVelocity
+        computerPaddleYPosition = GAME_AREA_HEIGHT - PADDLE_HEIGHT
     } else if (computerPaddleYPosition < 0) {
-        computerPaddleYVelocity = -computerPaddleYVelocity
+        computerPaddleYPosition = 0
     }
-
 
     // CPU movement update
     computerPaddleYPosition += computerPaddleYVelocity
@@ -137,9 +143,9 @@ function update() {
 
     // Update the player paddle's position based on input
     if (input_Down) {
-        playerPaddleYVelocity = 1
+        playerPaddleYVelocity = PADDLE_SPEED
     } else if (input_Up) {
-        playerPaddleYVelocity = -1
+        playerPaddleYVelocity = -PADDLE_SPEED
     } else {
         playerPaddleYVelocity = 0
     }
